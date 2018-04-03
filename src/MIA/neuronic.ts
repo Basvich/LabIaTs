34bc6b9f903937e7da9e68f94b2a58660b687b6d
Basilio <basilio.alvarez@gmail.com>
2018-04-04 00:11:22 +0200

    In the route
    Not capable to learn

--- src/MIA/neuronic.ts
+++ src/MIA/neuronic.ts
@@ -1,34 +1,94 @@
 import { randomBytes } from "crypto";
+import { isNumber } from "util";
 
 //import { NOTIMP } from "dns";
 
-
-type TransFunc= (n: number) => number;
+type TransFunc = (n: number) => number;
 
 export class TNeuron {
-  public a: number;
-  public bias=0.5;
-  public inputs: Array<number>;
+
+  protected funcSum;
+
+  protected funcGetIns;
+  /** Calor calculado de la neurona
+   * @type {number}
+   * @memberof TNeuron
+   */
+  public y:number;
+  public bias = 0.5;
+  public inputs: Array<number | TNeuron>;
   public weigths: Array<number>;
-  public transf: TransFunc=tranferStep;
+  public transf: TransFunc = tranferStep;
 
-  constructor(nInputs:number){
-    this.inputs=new Array(nInputs);
-    this.weigths=new Array(nInputs);
-    const w=1/nInputs;
-    for(let i=0; i<nInputs;i++){
-      this.weigths[i]=w;
+  constructor(ins:Array<TNeuron | number>) {
+    if(!ins) return;
+    this.buildFrom(ins);
+  }
+
+  protected sumFromNumbers(ins:Array<number>):number{
+     let res=0;
+     for(let i=0;i<this.inputs.length;i++){
+       res+=(<number>this.inputs[i])*this.weigths[i];
+     }
+     return  res;
+  }
+
+  protected sumFromNeu(ins:Array<TNeuron>){
+    let res=0;
+     for(let i=0;i<this.inputs.length;i++){
+       res+=(<TNeuron>this.inputs[i]).y*this.weigths[i];
+     }
+     return  res;
+  }
+
+  protected getInNeu(ins:TNeuron){
+    return  ins.y;
+  }
+
+  protected getInNum(ins:number){
+    return  ins;
+  }
+
+  public buildFrom(fromInputs:Array<number | TNeuron>){
+    if(!fromInputs || fromInputs.length===0) throw new Error('invalid inputs');
+    const isnNums= (typeof fromInputs[0] ==='number');
+    this.weigths=new Array(fromInputs.length);
+    for(let i=0;i<this.weigths.length;i++)
+       this.weigths[i]=0;
+    this.inputs=fromInputs;
+    //Segun el tipo de datos de entrada
+    if(isnNums) {
+      this.funcSum=this.sumFromNumbers;
+      this.funcGetIns=(a)=>a;
+    }else{
+      this.funcSum=this.sumFromNeu;
+     this.funcGetIns=(a:TNeuron)=>a.y;
     }
+    this.initRnd();
   }
+
+  /** Calcula la salida de la neurona
+   * @returns {number}
+   * @memberof TNeuron
+   */
   public calc(): number {
-    return this.a+this.bias;
+    let res = 0;
+    /*for (let i = 0; i < f; i++) {
+      res += this.inputs[i] * this.weigths[i];
+    }*/
+    res=this.funcSum();
+    res += this.bias;
+    this.y=res;
+    return this.transf(res);
   }
 
-  public showInfo():string{
-    return `[${this.weigths[0].toFixed(2)},${this.weigths[1].toFixed(2)}] bias:${this.bias.toFixed(2)}`;
+  public showInfo(): string {
+    return `[${this.weigths[0].toFixed(2)},${this.weigths[1].toFixed(
+      2
+    )}] bias:${this.bias.toFixed(2)}`;
   }
 
-  public calc2(): number{
+  /*public calc2(): number{
     const f=this.inputs.length;
     let res=0;
     for(let i= 0; i<f; i++){
@@ -36,20 +96,122 @@ export class TNeuron {
     }
     res+=this.bias;
     return this.transf(res);
-  }
+  }*/
 
-  public adjust(e:number, p:Array<number>){
-    for(let i=0; i<this.inputs.length; i++){
-      this.weigths[i]+=e*p[i];
+  /** Ajuste simple para un perceptron
+   * @param {number} e
+   * @param {Array<number>} p
+   * @memberof TNeuron
+   */
+  public adjust(e: number, p: Array<number>) {
+    for (let i = 0; i < this.inputs.length; i++) {
+      this.weigths[i] += e * p[i];
     }
-    this.bias=this.bias+e*1;
+    this.bias = this.bias + e * 1;
   }
 
-  public initRnd(){
-    if(this.weigths) this.weigths.forEach((value:number,index:number,array:number[])=>{array[index]=Math.random()*2-1;});
-    this.bias=0;
+  public adjustBackProp(lek:number){
+    if(!isFinite(lek)){
+      throw new Error('Invalid lek');
+    }
+    for(let i=0;i<this.inputs.length;i++){
+      const nW=this.weigths[i]+lek*this.funcGetIns(this.inputs[i]);
+      this.weigths[i]=nW;
+      if(isNaN(this.weigths[i])){
+        throw new Error('Invalid operation');
+      }
+    }
+    //El bias es como un peso con entrada 1
+    this.bias=this.bias+lek;
   }
 
+  public initRnd() {
+    if (this.weigths && this.weigths.length>0){
+      const ww=2;  //2/this.weigths.length;  //sirve para emprezar con pesos normalizados
+      const ofs=1;  //1/this.weigths.length;
+      this.weigths.forEach((value: number, index: number, array: number[]) => {
+        array[index] = Math.random() * ww - ofs;
+      });
+    }
+    this.bias =  Math.random() * 2 - 1;
+  }
+
+
+
+
+}
+
+export class TNeuronalNetwork {
+  public tasaAprendizage=0.3;
+}
+
+/** Red neuronal perceptron, con 2 capas (entradas y una neurona de salida)
+ * Funcion de transferencia igual para todas las neuronas.
+ * Tiene varias entradas y una única salida.
+ * Todas las entradas van a todas las neuronas
+ * @export
+ * @class TPerceptron1
+ */
+export class TPerceptron1 extends TNeuronalNetwork {
+  inputs: Array<number>;
+  layer: Array<TNeuron>;
+  /** La neurona de salida, conectada a todas las de entrada
+   * @type {TNeuron}
+   * @memberof TPerceptron1
+   */
+  neuY: TNeuron;
+
+  /** Calcula el resultado final de la red
+   *
+   * @returns {number}
+   * @memberof TPerceptron1
+   */
+  public calcY(): number {
+    if (!this.inputs) throw new Error('Not initialized');
+
+    this.layer.forEach(function(value: TNeuron) {
+      value.calc();
+    });
+    //Finalmente la neurona de salida
+    const y=this.neuY.calc();
+    return y;
+  }
+
+  public build(nImputs: number, nNeurons: number) {
+    this.inputs = new Array(nImputs);
+    for(let i=0;i<this.inputs.length;i++) this.inputs[i]=0;
+    this.layer = new Array(nNeurons);
+    for (let i = 0; i < nNeurons; i++) {
+      const n = new TNeuron(null);
+      n.buildFrom(this.inputs);
+      this.layer[i] = n;
+    }
+    this.neuY = new TNeuron(this.layer);
+
+  }
+
+  /** realiza un pazo de apredizaje comparado con
+   * el resultado actual
+   * @param {number} desired
+   * @memberof TPerceptron1
+   */
+  public learn(desired:number, currInputs:Array<number>){
+    for(let i=0;i<currInputs.length;i++) this.inputs[i]=currInputs[i];
+    this.calcY();  //Calculamos el resultado de la red
+    const error=desired-this.neuY.y;  //El error
+    //Cambio pesos neurona de salida. El 2 es por usar la funcion tanh
+    //Dk=error*g'(salida)
+    const Ek=error*2*this.neuY.y*(1-this.neuY.y);
+    const lek=this.tasaAprendizage*Ek;
+    this.neuY.adjustBackProp(lek);  //Cambio pesos neurona salida
+   //Cambio pesos capa interna (=entrada aqui)
+    for(let i=0;i<this.layer.length;i++){
+      const neu:TNeuron=this.layer[i];
+      const Eki=2*neu.y*(1-neu.y)*this.neuY.weigths[i]*Ek;
+      neu.adjustBackProp(this.tasaAprendizage*Eki);
+    }
+
+  }
 }
 
 /**
@@ -58,9 +220,8 @@ export class TNeuron {
  * @returns {number}
  */
 function tranferStep(a: number): number {
-  return (a<0)?-1: 1;
+  return a < 0 ? -1 : 1;
 }
-
 
 /** Funcion de transferencia con ligera
  * pendiente (continua)
@@ -68,10 +229,42 @@ function tranferStep(a: number): number {
  * @param {number} a
  * @returns {number}
  */
-function transferStep2(a:number):number{
-  if(a<-0.5) return -1;
-  if(a>0.5) return  1;
-  return  (2*a);
+function transferStep2(a: number): number {
+  if (a < -0.5) return -1;
+  if (a > 0.5) return 1;
+  return 2 * a;
 }
 
-export let FuncsTransfer=[{f:tranferStep, nfo:'Simple Step'},{f:transferStep2, nfo:'Slope step'}];
+/**
+ *
+ * Su derivada
+ * f1'(x)=f1(x)(1-f1(x));
+ * @param {number} a
+ * @returns
+ */
+function transferSigmoid(a:number){
+  //=1/(1+e^-x)
+
+  const c=1+Math.exp(-a);
+  return 1/c;
+}
+
+/** tangente hiperbolica (=sigmoide escalada)
+ * f2'(x)=2f1(x)(1-f1(x))=2f1'(x)
+ * @param {number} a
+ * @returns {number}
+ */
+function transferTanh(a: number): number {
+  //=(2/(1+e^-2x))-1=(1-e^-x)/(1+e^-x)
+  return Math.tanh(a);
+}
+
+function transferRelu(a: number): number {
+  return Math.max(0, a);
+}
+
+export let FuncsTransfer = [
+  { f: tranferStep, nfo: 'Simple Step' },
+  { f: transferStep2, nfo: 'Slope step' },
+  { f: transferTanh, nfo: 'tanh' }
+];
--- src/app/basics/double-neurone/double-neurone.component.ts
+++ src/app/basics/double-neurone/double-neurone.component.ts
@@ -1,6 +1,10 @@
 // Creado con ng generate component basics\doubleNeurone
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
+import { TSampleXY } from '../../dataTypes';
+import { ApplesService } from '../../apples.service';
+import { BCanvasComponent } from '../../b-comps/b-canvas/b-canvas.component';
+import { TNeuron, TPerceptron1, FuncsTransfer } from '../../../MIA/neuronic';
 
 
 @Component({
@@ -9,10 +13,113 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./double-neurone.component.css']
 })
 export class DoubleNeuroneComponent implements OnInit {
+  private lastIndex=0;
+  public data: TSampleXY[];
+  public perceptronNet: TPerceptron1;
 
-  constructor() { }
+  @ViewChild(BCanvasComponent) bCanvas: BCanvasComponent;
+
+  constructor(private appleService: ApplesService) { }
 
   ngOnInit() {
+    this.getHeroes();
+    this.perceptronNet=new TPerceptron1();
+    this.perceptronNet.build(2,4);
+  }
+
+  getHeroes(): void {
+    this.appleService.getApples(18).subscribe(apples => (this.data = apples));
+    //this.data = this.appleService.getApples();
+  }
+
+  public onLearnStep() {
+    const thats = this;
+    const index =  (this.lastIndex++) % this.data.length;// Math.floor(Math.random() * this.data.length);
+    const elemento: TSampleXY = this.data[index];
+    const din = [elemento.x, elemento.y];
+    const desired=thats.clasify(elemento);
+    this.perceptronNet.learn(desired, din);
+   /*  //Recorremos todos los samples
+    thats.neu1.inputs[0] = elemento.x;
+    thats.neu1.inputs[1] = elemento.y;
+    const rC = thats.neu1.calc2();
+    const rOk = thats.clasify(elemento);
+    let e = rOk - rC; //rC-rOk; //
+    e=e*0.5;
+    console.log(`(${elemento.x.toFixed(2)}, ${elemento.y.toFixed(2)}) rC:${rC} rOk:${rOk}`);
+    if (e !== 0) {
+      din[0] = elemento.x;
+      din[1] = elemento.y;
+      this.neu1.adjust(e, din);
+    }
+    console.log(thats.neu1.showInfo());*/
+    this.drawbackgroundNeu();
+    this.drawSamples();
+    //this.currentError=this.calcError2();
+  }
+
+  public drawbackgroundNeu() {
+    const imgData = this.bCanvas.context.createImageData(
+      this.bCanvas.widthPx,
+      this.bCanvas.heightPx
+    );
+    const v: DataView = new DataView(imgData.data.buffer);
+    const red = parseInt('FF0000FF', 16);
+    const blue = parseInt('00FFFFFF', 16);
+    const white = parseInt('FFFFFFFF', 16);
+    let index = 0;
+    const isx=1/this.bCanvas.scaleX;
+    const isy=1/this.bCanvas.scaleY;
+    for (let y = 0; y < imgData.height; y++) {
+      this.perceptronNet.inputs[1]= y*isy;
+      for (let x = 0; x < imgData.width; x++) {
+        this.perceptronNet.inputs[0] = x*isx;
+        const rC = this.perceptronNet.calcY();
+        if (rC < -0.5) {
+          v.setInt32(index, red);
+        } else {
+          if(rC>0.5){
+            v.setInt32(index,blue);
+          }else{
+            v.setInt32(index, white);
+          }
+        }
+        //const col=(x<50)?red:blue;
+        //v.setFloat32(index,col);
+        index += 4;
+      }
+    }
+    this.bCanvas.context.putImageData(imgData, 0, 0);
+  }
+
+
+  public drawSamples(){
+    const canvas = this.bCanvas;
+    const thats = this;
+    this.data.forEach(function(value: TSampleXY) {
+      //thats.drawApple(value);
+      const color= (thats.clasify(value)>0)?'blue':'red';
+      thats.drawSample(value,color);
+    });
+
+  }
+
+
+  protected drawSample(s:TSampleXY, color: string | CanvasGradient | CanvasPattern){
+    this.bCanvas.context.beginPath();
+    this.bCanvas.context.rect((s.x*this.bCanvas.scaleX)-4,(s.y*this.bCanvas.scaleY)-4,8,8);
+    this.bCanvas.context.fillStyle= color;
+    this.bCanvas.context.fill();
+    this.bCanvas.context.strokeStyle= 'black';
+    this.bCanvas.context.stroke();
+  }
+
+
+  //Clasifica manualmente las manzanas segun esas medidas en el tipo 0 o 1
+  public clasify(apple: TSampleXY): number {
+    let b=apple.x>0.35;
+    b=b && apple.y>0.35;
+    return b?1:-1;
   }
 
 }
