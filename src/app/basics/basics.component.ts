baaf0e77f4fbc07a334751a0acf696218888e3c2
Basilio <basilio.alvarez@gmail.com>
2018-07-05 23:56:31 +0200

    Ajustados menus principales

--- src/app/basics/basics.component.ts
+++ src/app/basics/basics.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   template:  `
-    <h3>Basics</h3>
     <nav>
       <a routerLink="./simpleNeuron" routerLinkActive="active">Una neurona</a>
       <a routerLink="./doubleNeuron" routerLinkActive="active">Dos neuronas</a>
